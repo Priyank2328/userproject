6fba847f5a89d0e14d2b015225757f4a7d78b778
Priyank2328 <shahpriyank7880@gmail.com>
2022-09-29 16:01:53 +0530

    second

--- lib/view/Screen2.dart
+++ lib/view/Screen2.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:userproject/models/Users.dart';
-
 class Screen2 extends StatefulWidget {
   Users obj;
   Screen2({this.obj});
   @override
   State<Screen2> createState() => _Screen2State();
 }
-
 class _Screen2State extends State<Screen2> {
   @override
   Widget build(BuildContext context) {
